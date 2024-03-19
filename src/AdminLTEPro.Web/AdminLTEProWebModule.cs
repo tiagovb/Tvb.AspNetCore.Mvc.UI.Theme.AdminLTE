afd454f994655ad4bff5549b4903c19c4c2cdd14
Tiago Viana <tiagovianabatista@gmail.com>
2024-03-19 11:07:08 -0300

    Update net8.0

--- src/AdminLTEPro.Web/AdminLTEProWebModule.cs
+++ src/AdminLTEPro.Web/AdminLTEProWebModule.cs
@@ -38,201 +38,200 @@ using Volo.Abp.UI.Navigation;
 using Volo.Abp.VirtualFileSystem;
 using Abp.AspNetCore.Mvc.UI.Theme.AdminLTE;
 
-namespace AdminLTEPro.Web
+namespace AdminLTEPro.Web;
+
+[DependsOn(
+    typeof(AdminLTEProHttpApiModule),
+    typeof(AdminLTEProApplicationModule),
+    typeof(AdminLTEProEntityFrameworkCoreDbMigrationsModule),
+    typeof(AbpAutofacModule),
+    typeof(AbpIdentityWebModule),
+    typeof(AbpAccountWebIdentityServerModule),
+    // typeof(AbpAspNetCoreMvcUiBasicThemeModule),
+    typeof(AbpAspNetCoreMvcUiAdminLTEThemeModule),
+    typeof(AbpAspNetCoreAuthenticationJwtBearerModule),
+    typeof(AbpTenantManagementWebModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpSwashbuckleModule)
+    )]
+public class AdminLTEProWebModule : AbpModule
 {
-    [DependsOn(
-        typeof(AdminLTEProHttpApiModule),
-        typeof(AdminLTEProApplicationModule),
-        typeof(AdminLTEProEntityFrameworkCoreDbMigrationsModule),
-        typeof(AbpAutofacModule),
-        typeof(AbpIdentityWebModule),
-        typeof(AbpAccountWebIdentityServerModule),
-        // typeof(AbpAspNetCoreMvcUiBasicThemeModule),
-        typeof(AbpAspNetCoreMvcUiAdminLTEThemeModule),
-        typeof(AbpAspNetCoreAuthenticationJwtBearerModule),
-        typeof(AbpTenantManagementWebModule),
-        typeof(AbpAspNetCoreSerilogModule),
-        typeof(AbpSwashbuckleModule)
-        )]
-    public class AdminLTEProWebModule : AbpModule
+    public override void PreConfigureServices(ServiceConfigurationContext context)
     {
-        public override void PreConfigureServices(ServiceConfigurationContext context)
+        context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
         {
-            context.Services.PreConfigure<AbpMvcDataAnnotationsLocalizationOptions>(options =>
-            {
-                options.AddAssemblyResource(
-                    typeof(AdminLTEProResource),
-                    typeof(AdminLTEProDomainModule).Assembly,
-                    typeof(AdminLTEProDomainSharedModule).Assembly,
-                    typeof(AdminLTEProApplicationModule).Assembly,
-                    typeof(AdminLTEProApplicationContractsModule).Assembly,
-                    typeof(AdminLTEProWebModule).Assembly
-                );
-            });
-        }
+            options.AddAssemblyResource(
+                typeof(AdminLTEProResource),
+                typeof(AdminLTEProDomainModule).Assembly,
+                typeof(AdminLTEProDomainSharedModule).Assembly,
+                typeof(AdminLTEProApplicationModule).Assembly,
+                typeof(AdminLTEProApplicationContractsModule).Assembly,
+                typeof(AdminLTEProWebModule).Assembly
+            );
+        });
+    }
 
-        public override void ConfigureServices(ServiceConfigurationContext context)
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        ConfigureUrls(configuration);
+        ConfigureBundles();
+        ConfigureAuthentication(context, configuration);
+        ConfigureAutoMapper();
+        ConfigureVirtualFileSystem(hostingEnvironment);
+        ConfigureLocalizationServices();
+        ConfigureNavigationServices();
+        ConfigureAutoApiControllers();
+        ConfigureSwaggerServices(context.Services);
+    }
+
+    private void ConfigureUrls(IConfiguration configuration)
+    {
+        Configure<AppUrlOptions>(options =>
         {
-            var hostingEnvironment = context.Services.GetHostingEnvironment();
-            var configuration = context.Services.GetConfiguration();
+            options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+        });
+    }
 
-            ConfigureUrls(configuration);
-            ConfigureBundles();
-            ConfigureAuthentication(context, configuration);
-            ConfigureAutoMapper();
-            ConfigureVirtualFileSystem(hostingEnvironment);
-            ConfigureLocalizationServices();
-            ConfigureNavigationServices();
-            ConfigureAutoApiControllers();
-            ConfigureSwaggerServices(context.Services);
-        }
-
-        private void ConfigureUrls(IConfiguration configuration)
+    private void ConfigureBundles()
+    {
+        Configure<AbpBundlingOptions>(options =>
         {
-            Configure<AppUrlOptions>(options =>
-            {
-                options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
-            });
-        }
-
-        private void ConfigureBundles()
-        {
-            Configure<AbpBundlingOptions>(options =>
-            {
-                options.StyleBundles.Configure(
-                    BasicThemeBundles.Styles.Global,
-                    bundle =>
-                    {
-                        bundle.AddFiles("/global-styles.css");
-                    }
-                );
-            });
-        }
-
-        private void ConfigureAuthentication(ServiceConfigurationContext context, IConfiguration configuration)
-        {
-            context.Services.AddAuthentication()
-                .AddJwtBearer(options =>
+            options.StyleBundles.Configure(
+                BasicThemeBundles.Styles.Global,
+                bundle =>
                 {
-                    options.Authority = configuration["AuthServer:Authority"];
-                    options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
-                    options.Audience = "AdminLTEPro";
-                });
-        }
-
-        private void ConfigureAutoMapper()
-        {
-            Configure<AbpAutoMapperOptions>(options =>
-            {
-                options.AddMaps<AdminLTEProWebModule>();
-            });
-        }
-
-        private void ConfigureVirtualFileSystem(IWebHostEnvironment hostingEnvironment)
-        {
-            if (hostingEnvironment.IsDevelopment())
-            {
-                Configure<AbpVirtualFileSystemOptions>(options =>
-                {
-                    var sept = Path.DirectorySeparatorChar;
-                    var rootPath = hostingEnvironment.ContentRootPath;
-                    options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProDomainSharedModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Domain.Shared"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProDomainModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Domain"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProApplicationContractsModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Application.Contracts"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProApplicationModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Application"));
-                    options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProWebModule>(rootPath);
-                    
-                    // Add theme virtual files
-                    options.FileSets.ReplaceEmbeddedByPhysical<AbpAspNetCoreMvcUiAdminLTEThemeModule>(Path.Combine(rootPath, $"..{sept}..{sept}theme{sept}Abp.AspNetCore.Mvc.UI.Theme.AdminLTE"));
-                });
-            }
-        }
-
-        private void ConfigureLocalizationServices()
-        {
-            Configure<AbpLocalizationOptions>(options =>
-            {
-                options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-                options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-                options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-                options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-                options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-                options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-                options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
-                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-                options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
-                options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch", "de"));
-                options.Languages.Add(new LanguageInfo("es", "es", "Español"));
-            });
-        }
-
-        private void ConfigureNavigationServices()
-        {
-            Configure<AbpNavigationOptions>(options =>
-            {
-                options.MenuContributors.Add(new AdminLTEProMenuContributor());
-            });
-        }
-
-        private void ConfigureAutoApiControllers()
-        {
-            Configure<AbpAspNetCoreMvcOptions>(options =>
-            {
-                options.ConventionalControllers.Create(typeof(AdminLTEProApplicationModule).Assembly);
-            });
-        }
-
-        private void ConfigureSwaggerServices(IServiceCollection services)
-        {
-            services.AddSwaggerGen(
-                options =>
-                {
-                    options.SwaggerDoc("v1", new OpenApiInfo { Title = "AdminLTEPro API", Version = "v1" });
-                    options.DocInclusionPredicate((docName, description) => true);
-                    options.CustomSchemaIds(type => type.FullName);
+                    bundle.AddFiles("/global-styles.css");
                 }
             );
-        }
+        });
+    }
 
-        public override void OnApplicationInitialization(ApplicationInitializationContext context)
-        {
-            var app = context.GetApplicationBuilder();
-            var env = context.GetEnvironment();
-
-            if (env.IsDevelopment())
+    private void ConfigureAuthentication(ServiceConfigurationContext context, IConfiguration configuration)
+    {
+        context.Services.AddAuthentication()
+            .AddJwtBearer(options =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseAbpRequestLocalization();
-
-            if (!env.IsDevelopment())
-            {
-                app.UseErrorPage();
-            }
-
-            app.UseCorrelationId();
-            app.UseStaticFiles();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseJwtTokenMiddleware();
-
-            if (MultiTenancyConsts.IsEnabled)
-            {
-                app.UseMultiTenancy();
-            }
-
-            app.UseIdentityServer();
-            app.UseAuthorization();
-            app.UseSwagger();
-            app.UseAbpSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "AdminLTEPro API");
+                options.Authority = configuration["AuthServer:Authority"];
+                options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
+                options.Audience = "AdminLTEPro";
             });
-            app.UseAuditing();
-            app.UseAbpSerilogEnrichers();
-            app.UseConfiguredEndpoints();
+    }
+
+    private void ConfigureAutoMapper()
+    {
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<AdminLTEProWebModule>();
+        });
+    }
+
+    private void ConfigureVirtualFileSystem(IWebHostEnvironment hostingEnvironment)
+    {
+        if (hostingEnvironment.IsDevelopment())
+        {
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                var sept = Path.DirectorySeparatorChar;
+                var rootPath = hostingEnvironment.ContentRootPath;
+                options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProDomainSharedModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Domain.Shared"));
+                options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProDomainModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Domain"));
+                options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProApplicationContractsModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Application.Contracts"));
+                options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProApplicationModule>(Path.Combine(rootPath, $"..{sept}AdminLTEPro.Application"));
+                options.FileSets.ReplaceEmbeddedByPhysical<AdminLTEProWebModule>(rootPath);
+
+                // Add theme virtual files
+                options.FileSets.ReplaceEmbeddedByPhysical<AbpAspNetCoreMvcUiAdminLTEThemeModule>(Path.Combine(rootPath, $"..{sept}..{sept}theme{sept}Abp.AspNetCore.Mvc.UI.Theme.AdminLTE"));
+            });
         }
+    }
+
+    private void ConfigureLocalizationServices()
+    {
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
+            options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
+            options.Languages.Add(new LanguageInfo("en", "en", "English"));
+            options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
+            options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
+            options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+            options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
+            options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
+            options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
+            options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
+            options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch", "de"));
+            options.Languages.Add(new LanguageInfo("es", "es", "Español"));
+        });
+    }
+
+    private void ConfigureNavigationServices()
+    {
+        Configure<AbpNavigationOptions>(options =>
+        {
+            options.MenuContributors.Add(new AdminLTEProMenuContributor());
+        });
+    }
+
+    private void ConfigureAutoApiControllers()
+    {
+        Configure<AbpAspNetCoreMvcOptions>(options =>
+        {
+            options.ConventionalControllers.Create(typeof(AdminLTEProApplicationModule).Assembly);
+        });
+    }
+
+    private void ConfigureSwaggerServices(IServiceCollection services)
+    {
+        services.AddSwaggerGen(
+            options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "AdminLTEPro API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            }
+        );
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAbpRequestLocalization();
+
+        if (!env.IsDevelopment())
+        {
+            app.UseErrorPage();
+        }
+
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseJwtTokenMiddleware();
+
+        if (MultiTenancyConsts.IsEnabled)
+        {
+            app.UseMultiTenancy();
+        }
+
+        app.UseIdentityServer();
+        app.UseAuthorization();
+        app.UseSwagger();
+        app.UseAbpSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "AdminLTEPro API");
+        });
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
     }
 }
